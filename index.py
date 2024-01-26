6bb6e1edb7b12d8a0eead8b2f5106d02b828f2e8
LeserraJ <leserrabusiness@gmail.com>
2024-01-26 21:19:44 +0000

    added some notes

---
+++ index.py
@@ -0,0 +1,11 @@
+#Data (Free or Buy) --Input-> Algorithim --Output(Trade Offer)-> Broker (Auto or Manual)
+
+#Algorithim is broken down into six components
+# 1. Universe Selection = Which Assest Class : Filters --> Liquidity, Price, Industry
+# 2. Alpha Model = Where algo assigns alpha scores to the stocks, trade signal is generated
+# 3. Portfolio Construction = Decides how much capital is allocated to each position
+# 4. Transaction Cost Model = Cost of opening new positions, slippage, market impact 
+# 5. Risk Model = Monitors risk and manages position
+# 6. Execution Model = Order Size, Type, Time, and Price
+
+
